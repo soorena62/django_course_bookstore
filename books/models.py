5bdfd10db149af8d1fc9110b19f7726a6e4f22a1
soorena62 <118964506+soorena62@users.noreply.github.com>
2023-08-11 05:22:55 +0330

    add BookListView

--- books/models.py
+++ books/models.py
@@ -2,13 +2,12 @@ from django.db import models
 
 
 class Book(models.Model):
-    author = models.CharField(max_length=200)
     title = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
         return self.title
-
 
 
