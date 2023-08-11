e44adaf7584d6092f4719f540a5e0423d558f043
soorena62 <118964506+soorena62@users.noreply.github.com>
2023-08-11 22:16:37 +0330

    add BookDetailView

--- books/models.py
+++ books/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     description = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
         return self.title
 
-
+    def get_absolute_url(self):
+        return reverse('book_detail', args=[self.id])
